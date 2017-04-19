1e94d1da55f7b6ec60f90f63cf8b6ca51ac3e880
Nick Jordan <andrewjord@gmail.com>
2017-04-19 19:05:04 -0400

    here ya go arny

---
+++ FileRead.java
@@ -0,0 +1,20 @@
+package utils;
+
+import Java.util.
+
+public class FileRead{
+	public FileRead(String filename){
+
+	}
+
+	public readFlightData(){
+
+	}
+
+	public readPathData(){
+
+	}
+	public output(){
+
+	}
+}
