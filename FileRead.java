103eebe76a3b4e3abc0dd35c61561d371dfb81c1
Nick Jordan <andrewjord@gmail.com>
2017-04-19 20:33:55 -0400

    well we tried to start..

--- FileRead.java
+++ FileRead.java
@@ -1,20 +1,33 @@
 package utils;
 
-import Java.util.
+import java.io.*;
+
 
 public class FileRead{
-	public FileRead(String filename){
+	public FileRead(String filename) throws FileNotFoundException{
+		System.out.println("HEllo");
+
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		try{
+			String line = br.readLine();
+			int numLines = Integer.parseInt(line);
+			while(numLines > 0){
+				System.out.println(line);
+			}
+		}catch(IOException e){
+			System.out.println("You dun goofed " + e.getMessage());
+			
+		}
+	}
+
+	public void readFlightData(){
 
 	}
 
-	public readFlightData(){
+	public void readPathData(){
 
 	}
-
-	public readPathData(){
-
-	}
-	public output(){
+	public void output(){
 
 	}
 }
--- main.java
+++ main.java
@@ -1,3 +1,44 @@
-class main{
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class main{
+	public static void main(String[] args){
+		//here we will run the app
+		
+		String arg1 = args[0];
+		//String arg2 = args[1];
+		//String arg3 = args[2];
+
+		System.out.println(arg1);
+		readPathInfo(arg1);
+	}
+
+	public static void readPathInfo(String arg1){
+		BufferedReader br = null;
+		FileReader fr = null;
+		try{
+			fr = new FileReader(arg1);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+			int numLines = Integer.parseInt(line);
+			System.out.println(numLines);
+			while((line = br.readLine()) != null){
+				System.out.println(line);
+			}
+
+		}catch(IOException e){
+
+		}
+	}
+
+	public void readFlightInfo(){
+
+	}
+
+	public void output(){
+
+	}
 }
